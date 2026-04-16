f127a345a072de28ae7cf76fc442be6067799674
Jonathan <jonathannguettia04@gmail.com>
2026-04-16 15:55:59 +0200

    feat: AfilieController

--- src/pages/affilie/AffiliePages.jsx
+++ src/pages/affilie/AffiliePages.jsx
@@ -146,10 +146,10 @@ export function AffilieDashboard() {
       .then(d => { setStats(d?.data || null); setLoading(false); })
       .catch(() => setLoading(false));
   }, []);
+
   useEffect(() => {
-    console.log("Composant Dashboard monté, tentative de fetch...");
-    fetchStats();
-  }, []); // Le tableau vide [] signifie "au chargement de la page"
+    console.log("Le Dashboard est bien chargé");
+  }, []);
 
   if (loading) return <Loader />;
 
