5749371e918fa4b1dafe81369caf51ee86db7ca9
Jonathan <jonathannguettia04@gmail.com>
2026-04-23 17:53:54 +0200

    fix: force axios browser build

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    // Force vite 8 à utiliser l'entrée ESM d'axios
-    // évite le bug "axios.create is undefined" en production
-    mainFields: ["module", "main"],
+    alias: {
+      // Force la version browser d'axios
+      axios: "axios/dist/browser/axios.cjs",
+    },
   },
   optimizeDeps: {
     include: ["axios"],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/axios/, /node_modules/],
-      transformMixedEsModules: true,
-    },
   },
 });
