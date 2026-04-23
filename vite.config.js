b4fb4b6b602cc30d3ac408a3b75afdb866ef0c96
Jonathan <jonathannguettia04@gmail.com>
2026-04-23 17:58:09 +0200

    fix: axios browser mainFields

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      // Force la version browser d'axios
-      axios: "axios/dist/browser/axios.cjs",
-    },
+    mainFields: ["browser", "module", "main"],
   },
   optimizeDeps: {
     include: ["axios"],
+    esbuildOptions: {
+      mainFields: ["browser", "module", "main"],
+    },
+  },
+  build: {
+    commonjsOptions: {
+      include: [/axios/, /node_modules/],
+      transformMixedEsModules: true,
+    },
   },
 });
